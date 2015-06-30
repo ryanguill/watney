ac8e9a957e722253cbae55970b069eebadfa7917
Ryan Guill <ryanguill@gmail.com>
2015-06-30 18:43:49 -0500

    lots of history work. upped max history length to 15K per channel. bug fixes, changed jshintrc to not worry about unused symbols.

--- plugins/history.js
+++ plugins/history.js
@@ -1,237 +1,228 @@
 'use strict';
 
 module.exports = (function(){
-    let _ = require('lodash');
-    let moment = require('moment');
-    let bot;
-    let redis;
-    let maxHistoryPerChannel;
-    let request = require('request');
+  const _ = require('lodash');
+  const moment = require('moment');
+  const request = require('request');
 
+  let bot;
+  let redis;
+  let maxHistoryPerChannel;
 
-/*
+  function createGist (channel, data, callback) {
 
-        bot.on('message', function(from, to, text, message) {
-            logMessage(to, message.nick, message.user, text);
+    data = _.map(data, function(line) {
+      try {
+        line = JSON.parse(line);
+        return moment(line.time).format('YYYY-MM-DDTHH:mm:ss[Z]') + ' @' + line.user + ' ' + line.text;
+      } catch (e) {
+        console.error(e);
+      }
+    });
 
-            if (bot.isChannelPaused(to)) return;
+    const filename = channel + '-log.txt';
 
-			if (to === bot.botName) {
-			    //they are talking to us in a private message, set to to be from
-			    to = from;
-			}
-
-            var parts = text.split(' ');
-            var channel = to;
-
-            if (parts[0] === '#tail') {
-                if (parts.length > 1) {
-                    if (parts[1] === '!len') {
-                        if (parts.length > 2) {
-                            channel = parts[2];
-                        }
-                        getHistoryCountForChannel(channel, function(err, data) {
-                            if (err) {
-                                console.error(err);
-                                return bot.say(to, err);
-                            }
-                            return bot.say(to, 'I have ' + data + ' lines of history for ' + channel);
-                        });
-                    } else if (parts[1] === '!max') {
-                        return bot.say(to, maxHistoryPerChannel);
-                    } else if (parts[1] === '!missed') {
-                        if (parts.length > 2) {
-                            channel = parts[2];
-                        }
-                        //get the last time I left
-                        var output = [];
-                        redis.hget(bot.botName + '.' + channel + '.lastleave', from.toLowerCase(), function(err, lastSeenTime) {
-                            if (lastSeenTime !== null) {
-                                var lastSeenDateString = new Date(_.parseInt(lastSeenTime));
-
-                                redis.lrange('channel_log.' + channel, 0, maxHistoryPerChannel, function(err, data){
-                                    if (err) {
-                                        console.error(err);
-                                        return bot.say(to, err);
-                                    }
-
-                                    _.each(data, function(line) {
-                                        var lineData = JSON.parse(line);
-                                        output.push(line);
-                                        if (lineData.time < lastSeenTime) {
-                                            return false;
-                                        }
-                                    });
-
-                                    if (output.length) {
-                                        createGist(channel, output, function (err, response, body) {
-                                            if (err) {
-                                                console.error(err);
-                                                return bot.say(to, err);
-                                            }
-                                            try {
-                                                var data = JSON.parse(body);
-                                                bot.say(to, from + ': Log of messages in ' + channel + ' since ' + lastSeenDateString.toLocaleString() + ': ' + data.html_url);
-                                            } catch (e) {
-                                                console.error(e);
-                                            }
-
-                                        });
-                                    } else {
-                                        bot.say(to, 'Sorry, there haven`t been any messages in ' + channel + ' since ' + lastSeenDateString.toLocaleString());
-                                    }
-
-                                });
-
-                            } else {
-                                bot.say(to, 'who are you again? (I don`t know when you last left, so I can`t give you what you have missed)');
-                            }
-                        });
-                    } else if (isNumber(parts[1])) {
-                        //assume to is channel and parts[1] is the number of lines
-                        getTailForChannel(to, _.parseInt(parts[1]), function (err, data) {
-                            if (err) {
-                                console.error(err);
-                                return bot.say(to, err);
-                            }
-
-                            if (data.length) {
-                                createGist(to, data, function (err, response, body) {
-                                    if (err) {
-                                        console.error(err);
-                                        return bot.say(to, err);
-                                    }
-                                    var data = JSON.parse(body);
-                                    bot.say(to, data.html_url);
-                                });
-                            } else {
-                                return bot.say(to, 'Sorry, I don`t have any data for channel: ' + to);
-                            }
-                        });
-                    } else if (parts.length > 2 && isNumber(parts[2])) {
-                        channel = parts[1];
-                        getTailForChannel(channel, _.parseInt(parts[2]), function (err, data) {
-                            if (err) {
-                                console.error(err);
-                                return bot.say(to, err);
-                            }
-
-                            if (data.length) {
-                                createGist(channel, data, function (err, response, body) {
-                                    if (err) {
-                                        console.error(err);
-                                        return bot.say(to, err);
-                                    }
-                                    var data = JSON.parse(body);
-                                    bot.say(to, data.html_url);
-                                });
-                            } else {
-                                return bot.say(to, 'Sorry, I don`t have any data for channel: ' + channel);
-                            }
-                        });
-                    } else {
-                        return bot.say(to, 'use #tail <number of lines> or #tail <channel> <number of lines>');
-                    }
-                } else {
-                    return bot.say(to, 'use #tail <number of lines> or #tail <channel> <number of lines>');
-                }
-            }
-        });
+    const formData = {
+      description: 'IRC Log from ' + channel,
+      public: false,
+      files: {}
     };
-*/
 
-    function createGist (channel, data, callback) {
+    formData.files[filename] = {content: data.reverse().join('\n')};
 
-        data = _.map(data, function(line) {
-            try {
-                line = JSON.parse(line);
-                return moment(line.time).format('YYYY-MM-DDTHH:mm:ss[Z]') + ' @' + line.user + ' ' + line.text;
-            } catch (e) {
-                console.error(e);
-            }
-        });
+    request({
+      method: 'POST',
+      url: 'https://api.github.com/gists',
+      headers: {'user-agent': 'https://github.com/atuttle/zoidbox'},
+      form: JSON.stringify(formData)}, callback);
+  }
 
-        var filename = channel + '-log.txt';
+  function isNumber (x) {
+    return !isNaN(_.parseInt(x));
+  }
 
-        var formData = {
-                description: 'IRC Log from ' + channel,
-                public: false,
-                files: {}
-            };
+  function logMessage (message, channel, user) {
+    user = user || {};
+    const data = {
+      userID: user.id,
+      user: user.name,
+      text: message.text,
+      time: _.now()
+    };
+    redis.lpush('channel_log.' + channel.name.toLowerCase(), JSON.stringify(data));
+    redis.ltrim('channel_log.' + channel.name.toLowerCase(), 0, maxHistoryPerChannel);
+  }
 
-        formData.files[filename] = {content: data.reverse().join('\n')};
+  function getTailForChannel (channelName, lines, callback) {
+    lines = lines - 1;
+    if (lines < 0) lines = 0;
+    redis.lrange('channel_log.' + channelName.toLowerCase(), 0, lines, callback);
+  }
 
-        request({
-            method: 'POST',
-            url: 'https://api.github.com/gists',
-            headers: {'user-agent': 'https://github.com/atuttle/zoidbox'},
-            form: JSON.stringify(formData)}, callback);
-    }
+  function getHistoryCountForChannel (channelName, callback) {
+    redis.llen('channel_log.' + channelName.toLowerCase(), callback);
+  }
 
-    function isNumber (x) {
-        return !isNaN(_.parseInt(x));
-    }
+  function tailCommand (message, channel, user) {
+    if (message.parts.length === 2 && isNumber(message.parts[1])) {
+      const len = _.parseInt(message.parts[1]);
+      getTailForChannel(channel.name, len, function (err, data) {
+        if (err) {
+          console.error(err);
+          return channel.send('error! ' + err);
+        }
 
-    function logMessage (message, channel, user) {
-      user = user || {};
-      var data = {
-        userID: user.id,
-        user: user.name,
-        text: message.text,
-        time: _.now()
-      };
-      redis.lpush('channel_log.' + channel.name, JSON.stringify(data));
-      redis.ltrim('channel_log.' + channel.name, 0, maxHistoryPerChannel);
-    }
-
-    function getTailForChannel (channel, lines, callback) {
-        lines = lines - 1;
-        if (lines < 0) lines = 0;
-        redis.lrange('channel_log.' + channel, 0, lines, callback);
-    }
-
-    function getHistoryCountForChannel (channel, callback) {
-        redis.llen('channel_log.' + channel, callback);
-    }
-
-    function tailCommand (message, channel, user) {
-      if (message.parts.length === 2 && isNumber(message.parts[1])) {
-        var len = _.parseInt(message.parts[1]);
-        getTailForChannel(channel.name, len, function (err, data) {
-          if (err) {
+        if (data.length) {
+          createGist(channel.name, data, function (err, response, body) {
+            if (err) {
               console.error(err);
               return channel.send('error! ' + err);
-          }
+            }
+            const data = JSON.parse(body);
+            channel.send(data.html_url);
+          });
+        } else {
+          channel.send('Sorry, I don`t have any data for channel: ' + channel.name);
+        }
+      });
+    }
+  }
 
-          if (data.length) {
-              createGist(channel.name, data, function (err, response, body) {
-                  if (err) {
-                    console.error(err);
-                    return channel.send('error! ' + err);
-                  }
-                  var data = JSON.parse(body);
-                  channel.send(data.html_url);
-              });
-          } else {
-              channel.send('Sorry, I don`t have any data for channel: ' + channel.name);
-          }
-        });
+  function tailNoArgs (message, channel, user) {
+    const len = 50;
+    getTailForChannel(channel.name, len, function (err, data) {
+      if (err) {
+        console.error(err);
+        return channel.send('error! ' + err);
       }
+
+      if (data.length) {
+        createGist(channel.name, data, function (err, response, body) {
+          if (err) {
+            console.error(err);
+            return channel.send('error! ' + err);
+          }
+          const data = JSON.parse(body);
+          channel.send(data.html_url);
+        });
+      } else {
+        channel.send('Sorry, I don`t have any data for channel: ' + channel.name);
+      }
+    });
+  }
+
+  function tailChannelCommand (message, channel, user) {
+
+    let [command, len, targetChannel, ...rest] = message.parts;
+    len = _.parseInt(len);
+
+    targetChannel = targetChannel.replace('<#','').replace('>','');
+    const channelLookup = bot.getChannelGroupOrDMByID(targetChannel);
+    if (!_.isUndefined(channelLookup)) {
+      targetChannel = channelLookup.name;
     }
 
-    return function init( _bot) {
-      bot = _bot;
-      redis = bot.redis;
-      maxHistoryPerChannel = _.parseInt(bot.conf.get('maxHistoryPerChannel'));
+    getTailForChannel(targetChannel, len, function (err, data) {
+      if (err) {
+        console.error(err);
+        return channel.send('error! ' + err);
+      }
 
-      bot.logMessage = logMessage;
+      if (data.length) {
+        createGist(targetChannel, data, function (err, response, body) {
+          if (err) {
+            console.error(err);
+            return channel.send('error! ' + err);
+          }
+          const data = JSON.parse(body);
+          channel.send(data.html_url);
+        });
+      } else {
+        channel.send('Sorry, I don`t have any data for channel: #' + targetChannel);
+      }
+    });
 
-      bot.register({pattern: {},
-        f: logMessage,
-        type: 'IN'});
+  }
 
-      bot.register({pattern: {command: '!tail'},
-        f: tailCommand,
-        type: 'OUT'});
-    };
+  function tailLenChannel (message, channel) {
+
+    let [command, subCommand, targetChannel, ...rest] = message.parts;
+    //bot.log(command, subCommand, targetChannel, rest);
+    targetChannel = targetChannel.replace('<#','').replace('>','');
+    const channelLookup = bot.getChannelGroupOrDMByID(targetChannel);
+    if (!_.isUndefined(channelLookup)) {
+      targetChannel = channelLookup.name;
+    }
+
+    getHistoryCountForChannel(targetChannel, function(err, data) {
+      if (data > 0) {
+        return channel.send('I have ' + data + ' lines of history for #' + targetChannel);
+      } else {
+        return channel.send('I don`t have any data for #' + targetChannel);
+      }
+    });
+  }
+
+  function tailLen (message, channel) {
+
+    getHistoryCountForChannel(channel.name, function(err, data) {
+      if (data > 0) {
+        return channel.send('I have ' + data + ' lines of history for #' + channel.name);
+      } else {
+        return channel.send('I don`t have any data for #' + channel.name);
+      }
+    });
+  }
+
+  function tailMax (message, channel) {
+    return channel.send('I am configured to store ' + maxHistoryPerChannel + ' messages per channel.');
+  }
+
+  return function init( _bot) {
+    bot = _bot;
+    redis = bot.redis;
+    maxHistoryPerChannel = _.parseInt(bot.conf.get('maxHistoryPerChannel'));
+
+    bot.logMessage = logMessage;
+
+    bot.register({pattern: {},
+      f: logMessage,
+      type: 'IN'});
+
+    bot.register({pattern: {regex: new RegExp('!tail !len$')},
+      f: tailLen,
+      type: 'OUT',
+      flags: {stop: true}
+    });
+
+    bot.register({pattern: {regex: new RegExp('!tail !len .*')},
+      f: tailLenChannel,
+      type: 'OUT',
+      flags: {stop: true}
+    });
+
+    bot.register({pattern: {regex: new RegExp('!tail !max$')},
+      f: tailMax,
+      type: 'OUT',
+      flags: {stop: true}
+    });
+
+    bot.register({pattern: {regex: new RegExp('!tail [0-9]+$')},
+      f: tailCommand,
+      type: 'OUT',
+      flags: {stop: true}
+    });
+
+    bot.register({pattern: {regex: new RegExp('!tail [0-9]+ .*$')},
+      f: tailChannelCommand,
+      type: 'OUT',
+      flags: {stop: true}
+    });
+
+    bot.register({pattern: {regex: new RegExp('!tail$')},
+      f: tailNoArgs,
+      type: 'OUT',
+      flags: {stop: true}
+    });
+  };
 })();
