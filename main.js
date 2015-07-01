fb717d50353b4fc94dd6919d4153e2098d621ba3
Adam Tuttle <adam@fusiongrokker.com>
2015-07-01 08:52:34 -0400

    fix indentation in main.js (tabs)

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 
 (function bootstrap(){
 
-    require('babel/register')({
-      // Setting this will remove the currently hooked extensions of .es6, `.es`, `.jsx`
-      // and .js so you'll have to add them back if you want them to be used again.
-      extensions: ['.es6', '.es', '.jsx', '.js']
-    });
+	require('babel/register')({
+		// Setting this will remove the currently hooked extensions of .es6, `.es`, `.jsx`
+		// and .js so you'll have to add them back if you want them to be used again.
+		extensions: ['.es6', '.es', '.jsx', '.js']
+	});
 
 
-  var _ = require('lodash');
+	var _ = require('lodash');
 	var conf = require('nconf')
 		.argv()
 		.env()
@@ -20,7 +20,7 @@
 		});
 
 	var bot = initSlack( conf );
-  bot.login();
+	bot.login();
 
 	bot.setMaxListeners(20);
 	bot.use( require('./lib/core') );
@@ -44,75 +44,74 @@
 		);
 
 		b.conf = conf;
-    b.botName = conf.get('botName');
+		b.botName = conf.get('botName');
 
-    if (_.isUndefined(b.botName)) {
-      throw('you must define the botName in the config file!');
-    }
+		if (_.isUndefined(b.botName)) {
+			throw('you must define the botName in the config file!');
+		}
 
 		b.testingChannel = conf.get('testingChannel');
 		b.ignoreChannels = conf.get('ignoreChannels');
 
 
 		b.on('open', function() {
-      b.channelList = Object.keys(b.channels)
-        .map(function (k) { return b.channels[k]; })
-        .filter(function (c) { return c.is_member; })
-        .map(function (c) { return c.name; });
+			b.channelList = Object.keys(b.channels)
+				.map(function (k) { return b.channels[k]; })
+				.filter(function (c) { return c.is_member; })
+				.map(function (c) { return c.name; });
 
-      b.groupList = Object.keys(b.groups)
-        .map(function (k) { return b.groups[k]; })
-        .filter(function (g) { return g.is_open && !g.is_archived; })
-        .map(function (g) { return g.name; });
+			b.groupList = Object.keys(b.groups)
+				.map(function (k) { return b.groups[k]; })
+				.filter(function (g) { return g.is_open && !g.is_archived; })
+				.map(function (g) { return g.name; });
 
-      b.botName = b.self.name;
+			b.botName = b.self.name;
 
-      console.log('Welcome to Slack. You are ' + b.self.name + ' of ' + b.team.name);
+			console.log('Welcome to Slack. You are ' + b.self.name + ' of ' + b.team.name);
 
-      if (b.channelList.length > 0) {
-          console.log('You are in: ' + b.channelList.join(', '));
-      }
-      else {
-          console.log('You are not in any channels.');
-      }
+			if (b.channelList.length > 0) {
+				console.log('You are in: ' + b.channelList.join(', '));
+			} else {
+				console.log('You are not in any channels.');
+			}
 
-      if (b.groupList.length > 0) {
-         console.log('As well as: ' + b.groupList.join(', '));
-      }
+			if (b.groupList.length > 0) {
+				console.log('As well as: ' + b.groupList.join(', '));
+			}
 
 		});
 
 
-    b.use = function use (plugin){
-      plugin(bot);
-    };
+		b.use = function use (plugin){
+			plugin(bot);
+		};
 
-    b.loadPlugins = function loadPlugins() {
-      var plugins = [];
-      var walk = require('walk');
-      var walker = walk.walk('./plugins', { followLinks: false });
+		b.loadPlugins = function loadPlugins() {
+			var plugins = [];
+			var walk = require('walk');
+			var walker = walk.walk('./plugins', { followLinks: false });
 
-      walker.on('file', function(root, stat, next){
+			walker.on('file', function(root, stat, next){
 
-        if (_.contains(['.js', '.es6'], stat.name.slice(-3))){
-          console.log('loading plugin %s/%s', root, stat.name);
-          try {
-            bot.use(require(root + '/' + stat.name));
-            plugins.push(root + '/' + stat.name);
-          }catch (err){
-            console.error(err);
-            console.log('----------------------');
-          }
-        }
+				if (_.contains(['.js', '.es6'], stat.name.slice(-3))){
+					console.log('loading plugin %s/%s', root, stat.name);
+					try {
+						bot.use(require(root + '/' + stat.name));
+						plugins.push(root + '/' + stat.name);
+					}catch (err){
+						console.error(err);
+						console.log('----------------------');
+					}
+				}
 
-        next();
-      });
+				next();
+			});
 
-      walker.on('end', function(){
-        console.log('plugins loaded: %s', plugins);
-      });
-    };
+			walker.on('end', function(){
+				console.log('plugins loaded: %s', plugins);
+			});
+		};
 
-    return b;
+	 return b;
 	}
 })();
