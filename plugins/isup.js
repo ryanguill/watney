0362cdc4dc605dcb5c23457c641b8a5551d7dd55
Ryan Guill <ryanguill@gmail.com>
2015-06-30 19:17:55 -0500

    added isup plugin.

---
+++ plugins/isup.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const _ = require('lodash');
+const isup = require('is-up');
+const validator = require('valid-url');
+
+module.exports = (function(){
+
+	let _bot;
+
+  function checkurl (message, channel, user) {
+    let url = _.last(message.text.replace('^<', '').replace('>', '').split('|'));
+
+    if (_.isUndefined(url) || !validator.isWebUri( 'http://' + url )){
+      //fail silently since this doesn't appear to be a url
+      return;
+    }
+
+    isup(url, function(err, up){
+      channel.send(url + ' is ' + (up ? 'up' : 'down') + ' for me' + (up ? '...' : ' too.') );
+    });
+  }
+
+	return function init(bot) {
+		_bot = bot;
+
+    bot.register({
+      pattern: {regex: /\^[^\s-]{4,}$/g},
+      f: checkurl,
+      type: 'OUT'
+    });
+	};
+
+})();
