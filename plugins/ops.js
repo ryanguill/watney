301c903866e4078aa7c4604df89c090a1d5b5765
Ryan Guill <ryanguill@gmail.com>
2015-07-02 15:25:20 -0500

    resolves https://github.com/ryanguill/watney/issues/10

--- plugins/ops.js
+++ plugins/ops.js
@@ -92,6 +92,44 @@ module.exports = (function(){
 		});
 	}
 
+	function pauseChannel (message, channel, user) {
+
+		let [command, botname] = message.parts;
+
+		 //you should be able to default the value in the destructuring, and babel takes that, but intellij doesnt approve
+		botname = botname || '';
+
+		let slackUser = bot.getUserForMention(botname);
+		if (_.isUndefined(slackUser)) {
+			slackUser = bot.getUserByName(botname);
+		}
+
+		if (_.isUndefined(slackUser) || slackUser.name !== bot.botName) {
+			return channel.send('are you talking to me? use `' + command + ' @' + bot.botName + '`');
+		}
+
+		bot.pauseChannel(channel, user);
+	}
+
+	function playChannel (message, channel, user) {
+
+		let [command, botname] = message.parts;
+
+		 //you should be able to default the value in the destructuring, and babel takes that, but intellij doesnt approve
+		botname = botname || '';
+
+		let slackUser = bot.getUserForMention(botname);
+		if (_.isUndefined(slackUser)) {
+			slackUser = bot.getUserByName(botname);
+		}
+
+		if (_.isUndefined(slackUser) || slackUser.name !== bot.botName) {
+			return channel.send('are you talking to me? use `' + command + ' @' + bot.botName + '`');
+		}
+
+		bot.playChannel(channel, user);
+	}
+
 	return function init (_bot) {
 		bot = _bot;
 		redis = bot.redis;
@@ -121,6 +159,26 @@ module.exports = (function(){
 			pattern: {command: '!deop'},
 			f: removeOp,
 			type: 'OUT'
+		});
+
+		bot.register({
+			pattern: {command: '!pause'},
+			f: pauseChannel,
+			type: 'OUT'
+		});
+
+		bot.register({
+			pattern: {command: '!play'},
+			f: playChannel,
+			type: 'OUT',
+			flags: {ignorePaused: true}
+		});
+
+		bot.register({
+			pattern: {command: '!unpause'},
+			f: playChannel,
+			type: 'OUT',
+			flags: {ignorePaused: true}
 		});
 
 	};
