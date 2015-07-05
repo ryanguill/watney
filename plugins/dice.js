ba7093f530eb251a215ff685df5f0834743e4264
Nicholas Claaszen <nicholas@equinoxcoding.com>
2015-07-05 16:53:49 +0200

    Split the regex up in to two groups - reDice contains old regex and allows for multi-dice processing  - reMessage is used for message filtering and looks for !roll

--- plugins/dice.js
+++ plugins/dice.js
@@ -2,8 +2,9 @@ const _ = require('lodash');
 
 module.exports = (function(){
 
-	let bot;
-	let reDice = new RegExp(/([1-9][0-9]{0,2})(d|D)([1-9][0-9]{0,3})((\+|-|\*|\/)([0-9]{1,3}))?/g);
+	let bot,
+		reDice = new RegExp(/([1-9][0-9]{0,2})(d|D)([1-9][0-9]{0,3})((\+|-|\*|\/)([0-9]{1,3}))?/g),
+		reMessage = new RegExp(/(^(\!roll )[1-9][0-9]{0,2})(d|D)([1-9][0-9]{0,3})((\+|-|\*|\/)([0-9]{1,3}))?/g);
 
 	function rollDice (dice) {
 		let result = 0,
@@ -39,7 +40,7 @@ module.exports = (function(){
 		for (let element of aDiceRolls) {
 			let [die, operator, modifier] = element.split(/(\+|-|\*|\/)/g),
 				roll = rollDice(die);
-			if (aBits.length === 3) roll.result = diceCalculation(roll.result, operator, modifier);
+			if (operator) roll.result = diceCalculation(roll.result, operator, modifier);
 			returnMessage = returnMessage + ' ' + element + ' = ' + roll.result + ' [' + roll.rolls.toString() + ']';
 		}
 		channel.send(returnMessage);
@@ -49,7 +50,7 @@ module.exports = (function(){
 		bot = _bot;
 
 		bot.register({
-			pattern: {regex: reDice},
+			pattern: {regex: reMessage},
 			f: procDice,
 			type: 'OUT'
 		});
