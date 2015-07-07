e246407c5be298eebb2bace057b8bd8cfc4cf24f
Nicholas Claaszen <nicholas@equinoxcoding.com>
2015-07-07 10:04:59 +0200

    checkTarget & Regex - Changed the checkTarget function to use different, cooler syntax. - Included E, GTE and LTE checks.

--- plugins/dice.js
+++ plugins/dice.js
@@ -3,26 +3,19 @@ const _ = require('lodash');
 module.exports = (function(){
 
 	let bot,
-		reDice = /([1-9][0-9]{0,2})(d|D)([1-9][0-9]{0,3})((\+|-|\*|\/)([0-9]{1,3}))?( *\&(g|G|l|L)t\; *[0-9]*)?/g,
-		reMessage = /(^(\!roll )[1-9][0-9]{0,2})(d|D)([1-9][0-9]{0,3})((\+|-|\*|\/)([0-9]{1,3}))?/g;
+		reDice = /([1-9][0-9]{0,2})(d|D)([1-9][0-9]{0,3})((\+|-|\*|\/)([0-9]{1,3}))?( *(&[g|l]t;=?|=) *[0-9]*)?/g,
+		reMessage = /(^(!roll )[1-9][0-9]{0,2})(d|D)([1-9][0-9]{0,3})((\+|-|\*|\/)([0-9]{1,3}))?/g;
 
 	function checkTarget(result, quantifier, target) {
-		let text = '';
-		if (quantifier === '&gt;') {
-			if (result > target) {
-				text = ' Success! ' + result + ' is higher than ' + target;
-			} else {
-				text = ' Failure! ' + result + ' is lower than ' + target;
-			}
-		} else if (quantifier === '&lt;') {
-			if (result < target) {
-				text = ' Success! ' + result + ' is lower than ' + target;
-			} else {
-				text = ' Failure! ' + result + ' is higher than ' + target;
-			}
-		} else {
-			text = ' We just dont know!';
-		}
+		let text = ' Failure!',
+			quantifiers = {
+				'&gt;': (a,b) => a>b,
+				'&lt;': (a,b) => a<b,
+				'&gt;=': (a,b) => a>=b,
+				'&lt;=': (a,b) => a<=b,
+				'=': (a,b) => a===b
+			};
+		if (quantifiers[quantifier.toLowerCase()](parseFloat(result), parseFloat(target))) text = ' Success!';
 		return text;
 	}
 
@@ -32,7 +25,7 @@ module.exports = (function(){
 			aResults = [];
 		for (let i = 0; i < throws; i++){
 			let roll = Math.floor((Math.random() * sides) + 1);
-			result = result + roll;
+			result += roll;
 			aResults.push(roll);
 		}
 		return {
@@ -57,13 +50,13 @@ module.exports = (function(){
 			aResults = [],
 			returnMessage = bot.makeMention(user) + ': You rolled:';
 		for (let element of aDiceRolls) {
-			let [diePart, quantifier, target] = element.split(/(&[g|G|l|L]t;)/g),
+			let [diePart, quantifier, target] = element.split(/(&[g|l]t;=*|=)/g),
 				[die, operator, modifier] = diePart.split(/(\+|-|\*|\/)/g),
 				roll = rollDice(die);
 			if (operator) roll.result = diceCalculation(roll.result, operator, modifier);
+			roll.target = '';
 			if (quantifier && target) roll.target = checkTarget(roll.result, quantifier, target);
-			roll.target = roll.target || '';
-			returnMessage = returnMessage + ' ' + element + ' = ' + roll.result + ' [' + roll.rolls.toString() +
+			returnMessage += ' ( ' + element + ' ) = ' + roll.result + ' [' + roll.rolls.toString() +
 				']' + roll.target;
 		}
 		channel.send(returnMessage);
