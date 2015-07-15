248ff55c0805a45a96ac461b48f8936d3650cc49
Dan Skaggs <dan.skaggs@web-meister.com>
2015-07-15 07:41:29 -0400

    Adding some comments and fixing jscs errors

--- plugins/conversion.js
+++ plugins/conversion.js
@@ -6,81 +6,79 @@ module.exports = (function() {
 
 	function doConversion (message, channel) {
 
-        let [command, ...args] = message.parts;
-        let measurement, unit;
+		let [command, ...args] = message.parts;
+		let measurement, unit;
 
-        if(!args.length){
-            channel.send('Please provide something to convert.');
-        } else {
-            //Extract numeric from units
-            if(args.length > 1){
-                measurement = args[0];
-                unit = args[1];
-            } else {
-                let measurements = args[0].match(/^[\d.]+/g);
-                let units = args[0].match(/[^\d.]+/g);
+		if(!args.length){
+			channel.send('Please provide something to convert.');
+		} else {
+			//The user put a space between the measurement and the units
+			if(args.length > 1){
+				measurement = args[0];
+				unit = args[1];
+			} else {
 
-                if( !(measurements.length && units.length) ){
-                    channel.send( 'Unable to extract measurement and/or units from the given parameters');
-                } else {
-                    measurement = measurements[0];
-                    unit = units[0];
-                }
+				//Extract the measurement and units from a single string
+				let measurements = args[0].match(/^[\d.]+/g);
+				let units = args[0].match(/[^\d.]+/g);
 
-            }
+				if( !(measurements.length && units.length) ){
+					channel.send( 'Unable to extract measurement and/or units from the given parameters');
+				} else {
+					measurement = measurements[0];
+					unit = units[0];
+				}
 
-            switch (unit) {
-                case 'f':
-                    channel.send( measurement + ' degrees fahrenheit is approximately ' + convertFahrenheitToCelcius(measurement) + ' degrees celcius.' );
-                    break;
-                case 'c':
-                    channel.send( measurement + ' degrees celcius is approximately ' + convertCelciusToFahrenheit(measurement) + ' degrees fahrenheit.' );
-                    break;
+			}
 
-                case 'mm':
-                    channel.send( measurement + ' millimeters is approximately ' + convertMillimetersToInches(measurement) + ' inches.' );
-                    break;
+			switch (unit) {
+				case 'f':
+					channel.send( measurement + ' degrees fahrenheit is approximately ' + convertFahrenheitToCelcius(measurement) + ' degrees celcius.' );
+					break;
+				case 'c':
+					channel.send( measurement + ' degrees celcius is approximately ' + convertCelciusToFahrenheit(measurement) + ' degrees fahrenheit.' );
+					break;
 
-                case 'in':
-                    channel.send( measurement + ' inches is approximately ' + convertInchesToMillimeters(measurement) + ' millimeters.' );
-                    break;
+				case 'mm':
+					channel.send( measurement + ' millimeters is approximately ' + convertMillimetersToInches(measurement) + ' inches.' );
+					break;
 
-                case '"':
-                    channel.send( measurement + ' inches is approximately ' + convertInchesToMillimeters(measurement) + ' millimeters.' );
-                    break;
+				case 'in':
+					channel.send( measurement + ' inches is approximately ' + convertInchesToMillimeters(measurement) + ' millimeters.' );
+					break;
 
-                default:
-                    channel.send('I did not recognize the units you wanted me to convert from.');
-                    break;
-            }
+				case '"':
+					channel.send( measurement + ' inches is approximately ' + convertInchesToMillimeters(measurement) + ' millimeters.' );
+					break;
 
-        }
+				default:
+					channel.send('I did not recognize the units you wanted me to convert from.');
+					break;
+			}
 
+		}
 
+	}
 
+	function convertCelciusToFahrenheit(degrees){
 
+		return Math.round( ((degrees*9)/5)+32 );
 
-    }
+	}
 
-    function convertCelciusToFahrenheit(degrees){
+	function convertFahrenheitToCelcius(degrees){
 
-        return Math.round( ((degrees*9)/5)+32 );
+		return Math.round( ((degrees-32)*5)/9 );
 
-    }
+	}
 
-    function convertFahrenheitToCelcius(degrees){
+	function convertMillimetersToInches( mm ){
+		return Math.round(( (mm * 0.0393701) + 0.00001) * 100) / 100;
+	}
 
-        return Math.round( ((degrees-32)*5)/9 );
-
-    }
-
-    function convertMillimetersToInches( mm ){
-        return Math.round(( (mm * 0.0393701) + 0.00001) * 100) / 100;
-    }
-
-    function convertInchesToMillimeters( inches ){
-        return Math.round(( (inches * 25.4) + 0.00001) * 100) / 100;
-    }
+	function convertInchesToMillimeters( inches ){
+		return Math.round(( (inches * 25.4) + 0.00001) * 100) / 100;
+	}
 
 	return function init (_bot) {
 		bot = _bot;
