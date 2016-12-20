6c5ae947b9bb842f74ea2c30aa14a871290e438a
Ryan Guill <ryanguill@gmail.com>
2016-12-20 08:51:07 -0600

    mingo!

--- plugins/misc.js
+++ plugins/misc.js
@@ -31,8 +31,25 @@ module.exports = (function(){
 		return responses[_.random(0, responses.length - 1)].split('{from}').join(from);
 	}
 
+	function randomMingo (from) {
+		let responses = [
+			'_unstable_ - pcmag'
+			, 'There is _No timeline for Mingo`s full release_'
+			, '_small & fast_'
+			, ' wants you to _stahp, pls_'
+			, 'm.i.n.g.o. and mingo was his name-o'
+			, '2min have passed.  it is 12:28.  His name-o was still mingo'
+		];
+
+		return responses[_.random(0, responses.length - 1)].split('{from}').join(from);
+	}
+
 	function give8BallResponse (message, channel, user) {
 		channel.send(random8ball(bot.makeMention(user)));
+	}
+
+	function giveMingoResponse (message, channel, user) {
+		channel.send(randomMingo(bot.makeMention(user)));
 	}
 
 	function give8BallResponseUserCheck (message, channel, user) {
@@ -111,6 +128,11 @@ module.exports = (function(){
 		bot.register({
 			pattern: {command: '!8ball'},
 			f: give8BallResponse,
+			type: 'OUT'});
+
+		bot.register({
+			pattern: {command: '!mingo'},
+			f: giveMingoResponse,
 			type: 'OUT'});
 
 		bot.register({
